22c7a2ab4e76fe4317e294264c657148b13c3c89
Eva Gandia Belda <147039955+evagandiabelda@users.noreply.github.com>
2025-02-12 12:53:13 +0100

    S'implementa un Loader per a evitar mostrar el panell d'usuari durant uns segons mentre l'aplicació detecta si un usuari està autenticat o no.

--- app/panel-estandar/layout.tsx
+++ app/panel-estandar/layout.tsx
@@ -1,47 +1,60 @@
-import { Metadata } from "next";
-import SidebarPanel from "@/components/layout/panel/SidebarPanel";
+"use client";
 
-export const metadata: Metadata = {
-    title: 'Espacio personal',
-};
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import SidebarPanel from "@/components/layout/panel/SidebarPanel";
+import Loader from "@/components/ui/Loader";
 
 export default function PanelEstandarLayout({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
+    const { data: session, status } = useSession();
+    const router = useRouter();
+
+    // Redirigir a login si el usuario no está autenticado
+    useEffect(() => {
+        if (status === "unauthenticated") {
+            router.replace("/login");
+        }
+    }, [status, router]);
+
+    // Mientras se carga la sesión, mostramos un loader
+    if (status === "loading") {
+        return <Loader />;
+    }
+
+    if (!session) return null; // Evita renderizar contenido innecesario antes de la redirección
 
     return (
         <>
-
-            {/* CAJA MODO CLARO - (se separan por problemas al aplicar estilos) */}
-
-            <div className='w-full flex mobile:flex-col tablet:flex-row gap-0 mobile:justify-start tablet:justify-between align-start pt-[30px] pb-0 dark:hidden fondo-degradado1'>
-                {/* CAJA SIDEBAR: */}
-                <section className='h-full mobile:w-full tablet:w-fit laptop:w-full laptop:max-w-[360px] px-[2rem] mobile:py-0 tablet:py-5'>
+            {/* CAJA MODO CLARO */}
+            <div className="w-full flex mobile:flex-col tablet:flex-row gap-0 mobile:justify-start tablet:justify-between align-start pt-[30px] pb-0 dark:hidden fondo-degradado1">
+                {/* CAJA SIDEBAR */}
+                <section className="h-full mobile:w-full tablet:w-fit laptop:w-full laptop:max-w-[360px] px-[2rem] mobile:py-0 tablet:py-5">
                     <SidebarPanel usuario="estandar" />
                 </section>
 
-                {/* CAJA PRINCIPAL: */}
-                <section className='flex flex-col w-full h-full p-[4rem] pb-[6rem] rounded-t-[28px] tablet:rounded-t-none tablet:rounded-tl-[28px] bg-[var(--gris1)]'>
+                {/* CAJA PRINCIPAL */}
+                <section className="flex flex-col w-full h-full p-[4rem] pb-[6rem] rounded-t-[28px] tablet:rounded-t-none tablet:rounded-tl-[28px] bg-[var(--gris1)]">
                     {children}
                 </section>
             </div>
 
-            {/* CAJA MODO OSCURO - (se separan por problemas al aplicar estilos) */}
-
-            <div className='w-full flex mobile:flex-col tablet:flex-row gap-0 mobile:justify-start tablet:justify-between align-start pt-[30px] pb-0 hidden dark:flex bg-[var(--gris4)]'>
-                {/* CAJA SIDEBAR: */}
-                <section className='h-full mobile:w-full tablet:w-fit laptop:w-full laptop:max-w-[360px] px-[2rem] mobile:py-0 tablet:py-5'>
+            {/* CAJA MODO OSCURO */}
+            <div className="w-full flex mobile:flex-col tablet:flex-row gap-0 mobile:justify-start tablet:justify-between align-start pt-[30px] pb-0 hidden dark:flex bg-[var(--gris4)]">
+                {/* CAJA SIDEBAR */}
+                <section className="h-full mobile:w-full tablet:w-fit laptop:w-full laptop:max-w-[360px] px-[2rem] mobile:py-0 tablet:py-5">
                     <SidebarPanel usuario="estandar" />
                 </section>
 
-                {/* CAJA PRINCIPAL: */}
-                <section className='flex flex-col w-full h-full p-[4rem] pb-[6rem] rounded-t-[28px] tablet:rounded-t-none tablet:rounded-tl-[28px] bg-[var(--gris5)]'>
+                {/* CAJA PRINCIPAL */}
+                <section className="flex flex-col w-full h-full p-[4rem] pb-[6rem] rounded-t-[28px] tablet:rounded-t-none tablet:rounded-tl-[28px] bg-[var(--gris5)]">
                     {children}
                 </section>
             </div>
-
         </>
     );
 }
