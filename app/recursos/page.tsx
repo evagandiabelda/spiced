1d48cbb6505d1e1e51be240eba8de387a8f8c4c7
Eva Gandia Belda <147039955+evagandiabelda@users.noreply.github.com>
2025-04-10 19:57:40 +0200

    S'elimina la cridada a un component innecessari a la pàgina de Recursos (només per a proves), que estava causant errors.

--- app/recursos/page.tsx
+++ app/recursos/page.tsx
@@ -15,44 +15,6 @@ export default function PanelExperto() {
             </h1>
 
             <div className="p-5">
-                <Options
-                    tipo="dropdown"
-                    label="Ejemplo de dropdown"
-                    name="dropdown"
-                    opciones={[
-                        { id: "Opcion1", texto: "Opción 1" },
-                        { id: "Opcion2", texto: "Opción 2" },
-                        { id: "Opcion3", texto: "Opción 3" },
-                    ]}
-                />
-            </div>
-
-            <div className="p-5">
-                <Options
-                    tipo="checkbox"
-                    label="Ejemplo de grupo de checkboxes"
-                    opciones={[
-                        { id: "Opcion1", texto: "Opción 1" },
-                        { id: "Opcion2", texto: "Opción 2" },
-                        { id: "Opcion3", texto: "Opción 3" },
-                    ]}
-                />
-            </div>
-
-            <div className="p-5">
-                <Options
-                    tipo="radio"
-                    label="Ejemplo de grupo de radios"
-                    name="grupo-radios-1"
-                    opciones={[
-                        { id: "Opcion1", texto: "Opción 1" },
-                        { id: "Opcion2", texto: "Opción 2" },
-                        { id: "Opcion3", texto: "Opción 3" },
-                    ]}
-                />
-            </div>
-
-            <div className="p-5">
                 <NubeTags />
             </div>
 
