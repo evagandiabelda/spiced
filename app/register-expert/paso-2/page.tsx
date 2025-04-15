adf15138a5d49bc16e6cf64128a14a7d4d8fdb96
Eva Gandia Belda <147039955+evagandiabelda@users.noreply.github.com>
2025-04-15 20:27:41 +0200

    S'implementa el Pas 2 del procés de registre d'usuaris Expert.

--- app/register-expert/paso-2/page.tsx
+++ app/register-expert/paso-2/page.tsx
@@ -13,40 +13,37 @@ export default function Paso2() {
     const { setRegistroData } = useRegistro();
 
     // Datos que se piden en este paso:
-    const [foto, setFoto] = useState<File | null>(null);
+    const [titulaciones, setTitulaciones] = useState<File[]>([]);
 
     // URL definitiva de Cloudinary:
-    const [fotoURL, setFotoURL] = useState("");
+    const [titulacionesURL, setTitulacionesURL] = useState<string[]>([]);
 
     const handleFileChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
 
-        const file = event.target.files?.[0];
+        const files = event.target.files;
 
-        if (file) {
-            setFoto(file);
-        } else {
-            return;
-        }
+        if (!files) return;
 
-        const uploadData = new FormData();
-        uploadData.append("file", file);
+        const nuevosArchivos = Array.from(files);
+        setTitulaciones(prev => [...prev, ...nuevosArchivos]);
 
-        try {
+        for (const file of nuevosArchivos) {
+            const uploadData = new FormData();
+            uploadData.append("file", file);
 
-            // En la petición '/api/upload' se gestiona la subida de la imagen a Cloudinary:
-            const response = await fetch("/api/upload", {
-                method: "POST",
-                body: uploadData,
-            });
+            try {
+                const response = await fetch("/api/upload", {
+                    method: "POST",
+                    body: uploadData,
+                });
 
-            if (!response.ok) throw new Error("Error subiendo la imagen");
+                if (!response.ok) throw new Error("Error subiendo el archivo");
 
-            const data = await response.json();
-
-            setFotoURL(data.url);
-
-        } catch (error) {
-            console.error("Error al subir la imagen:", error);
+                const data = await response.json();
+                setTitulacionesURL(prev => [...prev, data.url]);
+            } catch (error) {
+                console.error("Error al subir el archivo:", error);
+            }
         }
     };
 
@@ -55,46 +52,64 @@ export default function Paso2() {
         e.preventDefault();
 
         setRegistroData({
-            foto: fotoURL,
+            listaTitulaciones: titulacionesURL,
         });
 
         router.push('/register-expert/paso-3');
     }
 
     return (
-        <form onSubmit={handleSubmit} className="w-full h-full flex flex-col items-center gap-8">
+        <form onSubmit={handleSubmit} className="w-full flex flex-col items-center gap-4">
             <div className='w-full text-center flex flex-col gap-6 px-col1'>
-                <h2 className='w-full'>Elige tu foto de perfil</h2>
-                <p>Haz click en el gatito para cargar tu foto.<br /> Si no quieres mostrar tu cara, se te asignará un gatito de oficio.</p>
+                <h2 className='w-full'>Títulos y certificaciones</h2>
+                <p>Acredita tu condición de profesional de la salud pública adjuntando tus titulaciones y/o certificados profesionales. Estos documentos son necesarios para garantizar la autenticidad de tu perfil y verificarte como usuario Experto.<br />
+                    Tus titulaciones NO serán visibles por el resto de usuarios de Spiced.</p>
             </div>
 
-            <div className="w-full flex flex-col justify-start items-center gap-8 pt-4">
+            <div className="w-full flex mobile:flex-col laptop:flex-row gap-16 pb-4 pt-8">
 
-                <label
-                    htmlFor="foto"
-                    className="w-[360px] h-[360px] rounded-[800px] cursor-pointer hover:scale-[1.03] transition ease-in-out"
-                >
+                {/* GRUPO CARD */}
+
+                <div className='w-full flex flex-col justify-between items-center text-center gap-8 p-12 rounded-[2rem] border-dashed border-2 border-[#b0aaaa]'>
                     <Image
-                        src={
-                            foto
-                                ? URL.createObjectURL(foto)
-                                : "/iconos/iconos-registro/gatitos/icono-gato-00.svg"
-                        }
-                        alt="foto de perfil"
-                        width={300}
-                        height={300}
-                        className="w-[300px] h-[300px] object-cover rounded-full"
+                        src="/iconos/iconos-otros/icono-camara.svg"
+                        alt='Icono cámara de fotos'
+                        width={80}
+                        height={80}
                     />
-                </label>
+                    <div className='flex flex-col gap-4'>
+                        <h2>Añade tus archivos</h2>
+                        <p>Puedes subir archivos en formato PDF, JPG o PNG.</p>
+                    </div>
+                    <input
+                        type="file"
+                        id="titulacion"
+                        accept=".pdf,image/jpeg,image/png"
+                        multiple
+                        className="hidden"
+                        onChange={handleFileChange}
+                    />
 
-                <input
-                    type="file"
-                    id="foto"
-                    name="foto"
-                    className="hidden"
-                    accept="image/*"
-                    onChange={handleFileChange}
-                />
+                    <label htmlFor="titulacion" className='opacity-100 cursor-pointer'>
+                        <Boton
+                            texto="Explorar"
+                            tamano="grande"
+                            jerarquia="primario"
+                        />
+                    </label>
+                </div>
+
+                {/* GRUPO SUBIDOS */}
+
+                <div className='w-full h-full flex flex-col gap-8'>
+                    <div className='w-full flex flex-col gap-4'>
+                        {titulaciones.map((archivo, index) => (
+                            <div key={index} className='text-sm text-gray-600'>
+                                {archivo.name}
+                            </div>
+                        ))}
+                    </div>
+                </div>
 
             </div>
 
@@ -102,14 +117,15 @@ export default function Paso2() {
 
             <div className="w-full flex flex-row justify-end items-center gap-4">
                 <Boton
-                    texto='Atrás'
-                    enlace='/register/paso-1'
+                    texto='Cancelar'
+                    enlace='/'
                     tamano='grande'
                     jerarquia='secundario'
                 />
                 <BotonSubmit
                     texto="Siguiente"
                     icon="/iconos/iconos-otros/icono-arrow-right.svg"
+                    disabled={titulacionesURL.length === 0}
                 />
             </div>
 
