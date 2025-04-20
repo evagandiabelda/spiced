1a68b36e8afaa981da22ae588c3e9717a9ebfa80
Eva Gandia Belda <147039955+evagandiabelda@users.noreply.github.com>
2025-04-20 17:45:59 +0200

    S'inclou el codi mínim a les pàgines de Denuncias i Verificaciones per a que no falle el deploy.

---
+++ app/panel-admin/verificaciones/page.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+export default function Verificaciones() {
+
+    const { status } = useSession();
+    const router = useRouter();
+
+    useEffect(() => {
+        if (status === "unauthenticated") {
+            router.push("/login"); // Redirigir si no está autenticado
+        }
+    }, [status, router]);
+
+
+    return (
+        <div className="w-full flex flex-col gap-10">
+
+            {/* CABECERA: */}
+            <div className="w-full flex flex-col gap-8">
+                <div className="w-full flex mobile:flex-col-reverse tablet:flex-row justify-between mobile:items-between tablet:items-center mobile:gap-12 tablet:gap-3">
+                    <h2>Verificaciones</h2>
+                </div>
+            </div>
+
+            {/* CONTENIDO: */}
+            <div className="w-full flex flex-col gap-6">
+
+            </div>
+
+        </div>
+    );
+}
